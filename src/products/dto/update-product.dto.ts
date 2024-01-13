2dfa1fa5260d9f67bf94f08dedda1dee139f7c99
ayushmaan <ayushmaanroy.12@gmail.com>
2024-01-13 19:12:08 +0530

    updated

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,5 +1,35 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from "class-validator";
+
 export class UpdateProductDto {
-  name?: string;
-  description?: string;
-  price?: number;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  sku: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0, { message: "Additional cost must be a non-negative number" })
+  additionalCost?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0, { message: "Stock count must be a non-negative number" })
+  stockCount?: number;
 }
